e8f7cc46096cfa344c2502c09be8d1e24d956f83
terquin <91898536+terquin@users.noreply.github.com>
2026-06-08 12:58:43 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,19 @@
-const CACHE_NAME = 'inspeccio-v1';
-const urlsToCache = [
-  './index.html',
-  './manifest.json'
-];
+const CACHE_NAME = 'inspeccio-v2';
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
-  self.skipWaiting();
+self.addEventListener('install', e => {
+    self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
+self.addEventListener('activate', e => {
+    e.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Retorna la versió de la memòria cau si existeix, si no, busca-la a internet
-        return response || fetch(event.request);
-      })
-  );
+self.addEventListener('fetch', e => {
+    // Al ser una aplicació d'una sola pàgina sense complexitat de servidors,
+    // fem que qualsevol crida que falli per manca de xarxa busqui l'arxiu a la memòria.
+    e.respondWith(
+        fetch(e.request).catch(() => {
+            return caches.match(e.request);
+        })
+    );
 });
